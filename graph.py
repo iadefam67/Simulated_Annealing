41611698cbf8ee7ecd2dbf290f123b75b343e4ed
Lynnae Bryan <lynnaeg@pdx.edu>
2019-05-29 14:38:13 -0700

    minor cleanup

--- graph.py
+++ graph.py
@@ -2,10 +2,6 @@
 import random
 
 # modified from Massey, CS-350
-
-#ok rep K as a list, K_prime is a list, and use edge count, since all we need is len K, K_prime and an edge count. Cost also doesn't need to be part of the object. 
-
-
 class GraphAL(object):
   def __init__(self, nvertices, edges):
     "Create an adjacency-list style graph, using sets."
@@ -21,10 +17,12 @@ class GraphAL(object):
     return f"GraphAL:\n Vertices: {self.nvertices}, Neighbors:{self.neighbors}"
 
 class Subgraph(object):
-  def __init__(self, Graph):
+  """Stores a list of subset of nodes from a GraphAL object, can be randomly generated when created"""
+  def __init__(self, Graph, random_subset=False):
     self.node_set = None
     self.nvertices= None
-    self.random_subset(Graph)
+    if random_subset == True:
+      self.random_subset(Graph)
   def random_subset(self, Graph):
     "Result should be a random subset of digits from 0 to |V| in a list"
     nodes = [x for x in range(Graph.nvertices)]
@@ -35,13 +33,11 @@ class Subgraph(object):
     self.nvertices= len(self.node_set)
   def __repr__(self):
     return f"Subgraph node list: {self.node_set}, Cardinality: {self.nvertices}"
-  
-G = GraphAL(5, [(1,2),(2,3),(3,4),(0,4),(1,4)])
-K = Subgraph(G)
-print(G)
-print(K)
 
 def count_edges(GraphAL, Subgraph):
+  if Subgraph.node_set == None:
+    print("No subgraph created. Run 'random_subset()'. ")
+    return False
   count = 0
   for i in Subgraph.node_set:
     for v in GraphAL.neighbors[i]:
@@ -50,11 +46,14 @@ def count_edges(GraphAL, Subgraph):
   assert count % 2 == 0
   return count/2
 
-print(count_edges(G, K))
-
 # check for induced edge:
 def edge_check(Graph, v1, v2):
   if v1 in Graph.neighbors[v2] and v2 in Graph.neighbors[v1]:
     return True
   else: return False
 
+G = GraphAL(5, [(1,2),(2,3),(3,4),(0,4),(1,4)])
+K = Subgraph(G, random_subset=True)
+print(G)
+print(K)
+print("K edges:", count_edges(G, K))
