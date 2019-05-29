cabbdf564f25c399a0d942f35ebbcb3d5dbcca1b
Lynnae Bryan <lynnaeg@pdx.edu>
2019-05-29 14:23:06 -0700

    made subgraph class, instead of storing everything in the GraphAL class. Built edge count method for subgraph class

--- graph.py
+++ graph.py
@@ -2,34 +2,55 @@
 import random
 
 # modified from Massey, CS-350
+
+#ok rep K as a list, K_prime is a list, and use edge count, since all we need is len K, K_prime and an edge count. Cost also doesn't need to be part of the object. 
+
+
 class GraphAL(object):
   def __init__(self, nvertices, edges):
-    "Create an adjacency-list sactually adjacency-set) graph."
+    "Create an adjacency-list style graph, using sets."
     self.nvertices = nvertices
     self.neighbors = [set() for _ in range(nvertices)]
-    self.subgraph_K = None
-    self.nvertices_K = None
-    # how to keep track of edges in subgraph...
-    self.cost = float('inf')
+    #SA attributes
+    # build adjacency sets 
     for v1, v2 in edges:
       self.neighbors[v1].add(v2)
       self.neighbors[v2].add(v1)
-    self.random_subset()
-  def random_subset(self):
-    "Result should be a random subset of digits from 0 to |V| in a list"
-    nodes = [x for x in range(self.nvertices)]
-    # FIXME placeholder shuffle
-    random.shuffle(nodes)
-    N = random.randint(3, self.nvertices)
-    self.subgraph_K = set(nodes[:N])
-    self.nvertices_K = len(self.subgraph_K)
   def __repr__(self):
     "Print graph representation."
-    return f"GraphAL:\n Vert: {self.nvertices}, Neighbors:{self.neighbors}, Sub: {self.subgraph_K}, Sub num:{self.nvertices_K}"
+    return f"GraphAL:\n Vertices: {self.nvertices}, Neighbors:{self.neighbors}"
 
+class Subgraph(object):
+  def __init__(self, Graph):
+    self.node_set = None
+    self.nvertices= None
+    self.random_subset(Graph)
+  def random_subset(self, Graph):
+    "Result should be a random subset of digits from 0 to |V| in a list"
+    nodes = [x for x in range(Graph.nvertices)]
+    # FIXME placeholder shuffle
+    random.shuffle(nodes)
+    N = random.randint(3, Graph.nvertices)
+    self.node_set = set(nodes[:N])
+    self.nvertices= len(self.node_set)
+  def __repr__(self):
+    return f"Subgraph node list: {self.node_set}, Cardinality: {self.nvertices}"
+  
 G = GraphAL(5, [(1,2),(2,3),(3,4),(0,4),(1,4)])
+K = Subgraph(G)
 print(G)
+print(K)
 
+def count_edges(GraphAL, Subgraph):
+  count = 0
+  for i in Subgraph.node_set:
+    for v in GraphAL.neighbors[i]:
+      if v in Subgraph.node_set:
+        count += 1 
+  assert count % 2 == 0
+  return count/2
+
+print(count_edges(G, K))
 
 # check for induced edge:
 def edge_check(Graph, v1, v2):
