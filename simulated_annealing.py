dbf80bec78fcf6399e73725e791a6839995cb75c
Lynnae Bryan <lynnaeg@pdx.edu>
2019-06-05 18:30:46 -0700

    working on experiment script

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -90,31 +90,76 @@ def simulated_annealing(G, K, alpha, itr_per_t, max_itr, freeze):
   # if density_ratio(len(max_node_set), max_nedges) != 0:
     # print("search failed to converge on mIS")
   return (max_node_set, max_cost, max_nedges, itr)
+# ~~~~~~~~~~~~~~~~~~ LOCAL SEARCH ~~~~~~~~~~~~~~~~~~
+def naive_local_search(G, K, max_itr):
+  """Starts with subgraph K, and searches neighbor states. Only explores local optima. """
+  # generate starting solution
+  max_cost = float("-inf")
+  max_node_set = None
+  max_nedges = None
+  for i in range(max_itr):
+    # get neighbor solution
+    K_prime, K_prime_nvertices = neighbor_union_subtract(G, K)
+    K_prime_nedges = count_edges(G, K_prime)
+    K_cost = cost_dense(K.nvertices, K.nedges)
+    # calculate costs
+    K_prime_cost = cost_dense(K_prime_nvertices, K_prime_nedges)
+    cur_max_set, cur_max_cost, cur_nedges = ((K.node_set, K_cost, K.nedges) if K_cost > K_prime_cost else (K_prime, K_prime_cost, K_prime_nedges)) 
+    # track best solution found so far
+    if cur_max_cost > max_cost:
+      max_node_set = cur_max_set
+      max_nedges = cur_nedges
+      max_cost = cur_max_cost
+    # update K 
+    if K_prime_cost >= K_cost:
+      K.node_set = K_prime
+      K.nvertices = K_prime_nvertices
+      K.nedges = K_prime_nedges
+  return (max_node_set, max_cost, max_nedges, i)
 
-#FIXME 
+# ~~~~~~~~~~~~~~~~~~ RANDOM SEARCH ~~~~~~~~~~~~~~~~~~
+def random_search(G, K, max_itr):
+  """Explores possible randomly-generated subgraphs of G, returns best found solution for subgraph K. Not guaranteed to be MIS. """
+  best_K = K  
+  best_K_cost = cost_dense(K.nvertices, K.nedges)
+  for _ in range(max_itr):
+    K = Subgraph(G, random_subset=True)
+    K_cost = cost_dense(K.nvertices, K.nedges)
+    if K_cost > best_K_cost:
+      best_K = K
+      best_K_cost = K_cost
+  return K
+
+
+# ~~~~~~~~~~~~~~~~~~ EXPERIMENT SCRIPT ~~~~~~~~~~~~~~~~~~
+
 if (__name__ == '__main__'):
   # say 4 graphs of different sizes (test on 10, 15, 20, 25)
   # at 80% density
-  fp = open('./test_data.txt', 'w')
-  data = open('./per_run.txt', 'w')
-  num_runs = 5
-  graph_size_list = [5, 10, 15, 20]
-  for i in graph_size_list:
-    numerator = 0
-    edges = math.floor((i * (i - 1))/2 * .8)
-    nodes = i
-    for j in range(num_runs):
-      print(edges)
+  density_list = [.25, .5, .75]
+  for dp in density_list:
+    runtime_fp = open('./Data/test_data.txt', 'w')
+    data_fp = open(f'./Data/SA_run_data_{dp}.txt', 'w')
+    num_runs = 500
+    graph_size_list = [10, 20, 25, 30]
+    for i in graph_size_list:
+      numerator = 0
+      edges = math.floor((i * (i - 1))/2 * dp)
+      nodes = i
       e = nx.dense_gnm_random_graph(nodes, edges, seed = i)
       G = GraphAL(nodes, e.edges)
       K = Subgraph(G, random_subset=True)
-      start = time.time() 
-      maxns, maxcost, maxned, i = simulated_annealing(G, K, 1, ITR_PER_T, MAX_ITR, FREEZE)
-      total_sec = time.time() - start
-      numerator += total_sec
-      data.write(f'{maxcost}, {density_ratio(len(maxns), maxned)}, {i}\n')
-    # print(f'avg {numerator/num_runs}')
-    fp.write(f'{numerator/num_runs}\n')
+      
+      for j in range(num_runs):
+        start = time.time() 
+        maxns, maxcost, maxned, itr = simulated_annealing(G, K, 1, ITR_PER_T, MAX_ITR, FREEZE)
+        total_sec = time.time() - start
+        numerator += total_sec
+        data_fp.write(f'{i}, {maxcost}, {density_ratio(len(maxns), maxned)}, {itr}\n')
+      # print(f'avg {numerator/num_runs}')
+      runtime_fp.write(f'{i},{numerator/num_runs}\n')
+  runtime_fp.close()
+  data_fp.close()
 
 
 
